f707f9fd0b59af6305a9afc03eb7d9111baa8de4
Forrest Alvarez <forrest.alvarez@gmail.com>
2013-01-20 03:49:50 +0000

    updated json_youtube3x so that the script actually works on python 3

--- json_youtube3x.py
+++ json_youtube3x.py
@@ -7,10 +7,8 @@ url = 'http://gdata.youtube.com/feeds/api/standardfeeds/most_popular?v=2&alt=jso
 
 if __name__ == "__main__":
     pullUrl = urllib.request.urlopen(url)
-    openIt = urllib.request.build_opener()
-    openIt.addheaders = ['utf-8']
-    f = openIt.open(url)
-    jsong = json.load(f)
+    openIt = pullUrl.readall().decode('utf-8')
+    jsong = json.loads(openIt)
     topVidList = []
     for item in jsong['feed']['entry']:
         topVidList.append(item.get('title').get('$t'))
